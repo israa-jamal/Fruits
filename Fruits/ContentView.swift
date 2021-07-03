9f5f4abb01764d69c4127f76b2df267bb82ff91d
Esraa Gamal <esraa.gamal98@gmail.com>
2021-07-03 18:09:29 +0200

    Initial Commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by Esraa Gamal on 7/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
